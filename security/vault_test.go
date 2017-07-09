a9410c5a1beb814825c4336290059770cbb398d2
Roman <roman.atachiants@gmail.com>
2017-07-09 22:15:30 +0800

    added credentials reading to vault + tests

--- security/vault_test.go
+++ security/vault_test.go
@@ -17,6 +17,8 @@ func (h *testVaultHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		response = `{ "auth": {"client_token": "123" } }`
 	case "/v1/secret/test":
 		response = `{ "data": {"value": "hi" } }`
+	case "/v1/aws/sts/storage-emitter":
+		response = `{ "lease_duration": 100, "data": { "access_key": "access", "secret_key": "secret", "security_token": "token" } }`
 	}
 
 	w.Write([]byte(response))
@@ -33,7 +35,18 @@ func TestVaultClient(t *testing.T) {
 	assert.Equal(t, "123", cli.token)
 
 	// Test secret read now
-	v, err := cli.ReadSecret("test")
-	assert.NoError(t, err)
-	assert.Equal(t, "hi", v)
+	{
+		v, err := cli.ReadSecret("test")
+		assert.NoError(t, err)
+		assert.Equal(t, "hi", v)
+	}
+
+	// Test credentials read now
+	{
+		v, err := cli.ReadCredentials("storage-emitter")
+		assert.NoError(t, err)
+		assert.Equal(t, "access", v.AccessKey)
+		assert.Equal(t, "secret", v.SecretKey)
+		assert.Equal(t, "token", v.Token)
+	}
 }
