a26d97f8de9582855f418c3521b847a509bf8cea
Roman <roman.atachiants@gmail.com>
2017-07-10 21:43:24 +0800

    added some http tests

--- network/http/http_test.go
+++ network/http/http_test.go
@@ -3,10 +3,19 @@ package http
 import (
 	"bytes"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 )
+
+type testHandler struct{}
+
+type testObject struct {
+	Field string `json:"field"`
+}
 
 func TestUnmarshalJSON(t *testing.T) {
 	input := `{"test":"data","validation":"process"}`
@@ -22,4 +31,32 @@ func TestUnmarshalJSON(t *testing.T) {
 	}
 
 	assert.EqualValues(t, expected, actual)
+}
+
+func (h *testHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	b, _ := json.Marshal(&testObject{
+		Field: "response",
+	})
+	w.Write(b)
+}
+
+func TestPostGet(t *testing.T) {
+	s := httptest.NewServer(new(testHandler))
+	defer s.Close()
+	body := testObject{Field: "hello"}
+	expect := &testObject{Field: "response"}
+
+	{
+		output := new(testObject)
+		err := Post(s.URL, body, output)
+		assert.NoError(t, err)
+		assert.EqualValues(t, expect, output)
+	}
+
+	{
+		output := new(testObject)
+		err := Get(s.URL, output)
+		assert.NoError(t, err)
+		assert.EqualValues(t, expect, output)
+	}
 }
