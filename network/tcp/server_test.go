abca4ff2900e615a138f6631ec0230a56424fbf6
Roman <roman.atachiants@gmail.com>
2017-07-13 20:24:29 +0800

    tcp test

--- network/tcp/server_test.go
+++ network/tcp/server_test.go
@@ -1,1 +1,31 @@
 package tcp
+
+import (
+	"errors"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type mockListener struct{}
+
+func (l *mockListener) Accept() (net.Conn, error) {
+	return nil, errors.New("xxx")
+}
+
+func (l *mockListener) Close() error {
+	return nil
+}
+
+func (l *mockListener) Addr() net.Addr {
+	return nil
+}
+
+func TestServe(t *testing.T) {
+	s := new(Server)
+	defer s.Close()
+	err := s.Serve(new(mockListener))
+
+	assert.Error(t, err)
+}
