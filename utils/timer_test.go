9cbfd1986ab8880b92d09f2ceff8f71f013c9734
Roman <roman.atachiants@gmail.com>
2017-07-22 22:15:55 +0800

    timer test

---
+++ utils/timer_test.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRepeat(t *testing.T) {
+	closing := make(chan bool)
+	count := 0
+	defer close(closing)
+
+	Repeat(func() { count++ }, 1*time.Millisecond, closing)
+	time.Sleep(5 * time.Millisecond)
+	assert.True(t, count > 0)
+}
