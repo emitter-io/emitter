22c1483bc21b029f85595a3b6384f2dd437d1e4f
Roman <roman.atachiants@gmail.com>
2018-02-10 19:34:59 +0800

    Feature: channel-specific keys and matching, retro-compatible

--- security/key_test.go
+++ security/key_test.go
@@ -12,6 +12,41 @@ func TestKeyIsEmpty(t *testing.T) {
 	assert.True(t, true, key.IsEmpty())
 }
 
+func validateChannel(k Key, c string) bool {
+	return k.ValidateChannel(&Channel{Channel: []byte(c)})
+}
+
+func TestKey_New(t *testing.T) {
+	key := Key(make([]byte, 24))
+
+	// Test exact channel
+	key.SetTarget("a/b/c/")
+	assert.False(t, validateChannel(key, "a/b/"))
+	assert.True(t, validateChannel(key, "a/b/c/"))
+	assert.False(t, validateChannel(key, "a/b/c/d/"))
+
+	// Test exact channel with wildcard
+	key.SetTarget("a/+/c/")
+	assert.True(t, validateChannel(key, "a/b/c/"))
+	assert.True(t, validateChannel(key, "a/c/c/"))
+	assert.True(t, validateChannel(key, "a/d/c/"))
+	assert.True(t, validateChannel(key, "a/+/c/"))
+	assert.False(t, validateChannel(key, "a/b/+/"))
+
+	// Test open channel
+	key.SetTarget("a/b/c/#/")
+	assert.False(t, validateChannel(key, "a/b/"))
+	assert.True(t, validateChannel(key, "a/b/c/"))
+	assert.True(t, validateChannel(key, "a/b/c/d/"))
+	assert.True(t, validateChannel(key, "a/b/c/d/e/"))
+	assert.True(t, validateChannel(key, "a/b/c/d/+/f/"))
+	assert.True(t, validateChannel(key, "a/b/c/d/+/f/#/"))
+
+	assert.Nil(t, key.SetTarget("1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/"))
+	assert.Nil(t, key.SetTarget("1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/#/"))
+	assert.NotNil(t, key.SetTarget("1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/"))
+}
+
 func TestKey(t *testing.T) {
 	key := Key(make([]byte, 24))
 
@@ -20,7 +55,7 @@ func TestKey(t *testing.T) {
 	key.SetContract(123)
 	key.SetSignature(777)
 	key.SetPermissions(AllowReadWrite)
-	key.SetTarget(56789)
+	key.SetTarget("a/b/c/")
 	key.SetExpires(time.Unix(1497683272, 0).UTC())
 
 	assert.Equal(t, uint16(999), key.Salt())
@@ -28,7 +63,6 @@ func TestKey(t *testing.T) {
 	assert.Equal(t, uint32(123), key.Contract())
 	assert.Equal(t, uint32(777), key.Signature())
 	assert.Equal(t, AllowReadWrite, key.Permissions())
-	assert.Equal(t, uint32(56789), key.Target())
 	assert.Equal(t, time.Unix(1497683272, 0).UTC(), key.Expires())
 
 	key.SetExpires(time.Unix(0, 0))
