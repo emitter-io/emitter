4d9f9bff471c9c8f537e67a833495f47641ecf9e
Alain Gilbert <alain.gilbert.15@gmail.com>
2018-02-12 23:25:13 -0800

    Update github.com/emitter-io/config vendor to latest version

--- vendor/github.com/emitter-io/config/config.go
+++ vendor/github.com/emitter-io/config/config.go
@@ -9,7 +9,6 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"plugin"
 	"reflect"
 	"strconv"
 	"strings"
@@ -123,38 +122,7 @@ func (c *ProviderConfig) Load(builtins ...Provider) (Provider, error) {
 		}
 	}
 
-	// Attempt to load a plugin provider
-	p, err := plugin.Open(resolvePath(c.Provider))
-	if err != nil {
-		return nil, errors.New("The provider plugin '" + c.Provider + "' could not be opened. " + err.Error())
-	}
-
-	// Get the symbol
-	sym, err := p.Lookup("New")
-	if err != nil {
-		return nil, errors.New("The provider '" + c.Provider + "' does not contain 'func New() interface{}' symbol")
-	}
-
-	// Resolve the
-	pFactory, validFunc := sym.(*func() interface{})
-	if !validFunc {
-		return nil, errors.New("The provider '" + c.Provider + "' does not contain 'func New() interface{}' symbol")
-	}
-
-	// Construct the provider
-	provider, validProv := ((*pFactory)()).(Provider)
-	if !validProv {
-		return nil, errors.New("The provider '" + c.Provider + "' does not implement 'Provider'")
-	}
-
-	// Configure the provider
-	err = provider.Configure(c.Config)
-	if err != nil {
-		return nil, errors.New("The provider '" + c.Provider + "' could not be configured")
-	}
-
-	// Succesfully opened and configured a provider
-	return provider, nil
+	return c.LoadPlugin()
 }
 
 // LoadProvider loads a provider from the configuration or panics if the configuration is
