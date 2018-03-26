7ff0ccea24d25cc6056a5bc0e20e2846d816584f
Edoardo Rossi <e.rossi@emmedata.com>
2018-03-26 09:08:50 +0200

    update config pkg for internal certification autority

--- vendor/github.com/emitter-io/config/config.go
+++ vendor/github.com/emitter-io/config/config.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"plugin"
 	"reflect"
 	"strconv"
 	"strings"
@@ -40,17 +41,18 @@ type Config interface {
 
 // TLSConfig represents TLS listener configuration.
 type TLSConfig struct {
-	ListenAddr  string `json:"listen"`                // The address to listen on.
-	Host        string `json:"host"`                  // The hostname to whitelist.
-	Email       string `json:"email,omitempty"`       // The email address for autocert.
+	ListenAddr string `json:"listen"`          // The address to listen on.
+	Host       string `json:"host"`            // The hostname to whitelist.
+	Email      string `json:"email,omitempty"` // The email address for autocert.
 	Certificate string `json:"certificate,omitempty"` // The certificate request.
-	PrivateKey  string `json:"private,omitempty"`     // The private key for the certificate.
+	PrivateKey  string `json:"private,omitempty"`     // The private key for the certificate.	
 }
 
 // Load loads the certificates from the cache or the configuration.
 func (c *TLSConfig) Load(certCache autocert.Cache) (*tls.Config, error) {
+
 	if c.Certificate != "" {
-		if c.Certificate == "" || c.PrivateKey == "" {
+		if c.PrivateKey == "" {
 			return &tls.Config{}, errors.New("No certificate or private key configured")
 		}
 
@@ -157,7 +159,38 @@ func (c *ProviderConfig) Load(builtins ...Provider) (Provider, error) {
 		}
 	}
 
-	return c.LoadPlugin()
+	// Attempt to load a plugin provider
+	p, err := plugin.Open(resolvePath(c.Provider))
+	if err != nil {
+		return nil, errors.New("The provider plugin '" + c.Provider + "' could not be opened. " + err.Error())
+	}
+
+	// Get the symbol
+	sym, err := p.Lookup("New")
+	if err != nil {
+		return nil, errors.New("The provider '" + c.Provider + "' does not contain 'func New() interface{}' symbol")
+	}
+
+	// Resolve the
+	pFactory, validFunc := sym.(*func() interface{})
+	if !validFunc {
+		return nil, errors.New("The provider '" + c.Provider + "' does not contain 'func New() interface{}' symbol")
+	}
+
+	// Construct the provider
+	provider, validProv := ((*pFactory)()).(Provider)
+	if !validProv {
+		return nil, errors.New("The provider '" + c.Provider + "' does not implement 'Provider'")
+	}
+
+	// Configure the provider
+	err = provider.Configure(c.Config)
+	if err != nil {
+		return nil, errors.New("The provider '" + c.Provider + "' could not be configured")
+	}
+
+	// Succesfully opened and configured a provider
+	return provider, nil
 }
 
 // LoadProvider loads a provider from the configuration or panics if the configuration is
