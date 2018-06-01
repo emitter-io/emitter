0e0aaee5368ba4c4c496c2ffc351b582bea3e00b
Roman <roman.atachiants@gmail.com>
2018-06-01 21:26:25 +0800

    added unit tests

--- provider/storage/ssd_test.go
+++ provider/storage/ssd_test.go
@@ -26,14 +26,13 @@ import (
 
 	"github.com/emitter-io/emitter/message"
 	"github.com/emitter-io/stats"
+	"github.com/kelindar/binary"
 	"github.com/stretchr/testify/assert"
 )
 
 func getNTestMessages(count int) (frame message.Frame) {
-	//time := time.Now().Unix()
 	for i := 0; i < count; i++ {
 		id := message.NewID(message.Ssid{0, uint32(i / 2), 2, uint32(i)})
-		//id.SetTime(time + int64(i))
 		frame = append(frame, message.Message{
 			ID:      id,
 			Channel: []byte("test/channel/"),
@@ -44,13 +43,12 @@ func getNTestMessages(count int) (frame message.Frame) {
 	return
 }
 
-
 // Opens an NewSSD and runs a a test on it.
 func runSSDTest(test func(store *SSD)) {
 
 	// Prepare a store
 	dir, _ := ioutil.TempDir("", "emitter")
-	store := NewSSD()
+	store := NewSSD(nil)
 	store.Configure(map[string]interface{}{
 		"dir": dir,
 	})
@@ -62,14 +60,14 @@ func runSSDTest(test func(store *SSD)) {
 	test(store)
 }
 
-func TestSSDStore(t *testing.T) {
+func TestSSD_Store(t *testing.T) {
 	runSSDTest(func(store *SSD) {
 		err := store.storeFrame(getNTestMessages(10))
 		assert.NoError(t, err)
 	})
 }
 
-func TestSSDQuery(t *testing.T) {
+func TestSSD_Query(t *testing.T) {
 	runSSDTest(func(store *SSD) {
 		err := store.storeFrame(getNTestMessages(10))
 		assert.NoError(t, err)
@@ -81,7 +79,7 @@ func TestSSDQuery(t *testing.T) {
 	})
 }
 
-func TestQueryLast(t *testing.T) {
+func TestSSD_QueryOrdered(t *testing.T) {
 	runSSDTest(func(store *SSD) {
 		msgs := getNTestMessages(10)
 		err := store.storeFrame(msgs)
@@ -95,6 +93,87 @@ func TestQueryLast(t *testing.T) {
 		assert.Equal(t, message.Ssid{0, 3, 2, 7}, f[0].Ssid())
 
 	})
+}
+
+func TestSSD_QuerySurveyed(t *testing.T) {
+	runSSDTest(func(s *SSD) {
+		const wildcard = uint32(1815237614)
+		msgs := getNTestMessages(10)
+		s.storeFrame(msgs)
+		zero := time.Unix(0, 0)
+		tests := []struct {
+			query    []uint32
+			limit    int
+			count    int
+			gathered []byte
+		}{
+			{query: []uint32{0, 3, 2, 7}, limit: 10, count: 1},
+			{query: []uint32{0, 1}, limit: 5, count: 2},
+			{query: []uint32{0, 1}, limit: 5, count: 5, gathered: msgs.Encode()},
+		}
+
+		for _, tc := range tests {
+			if tc.gathered == nil {
+				s.cluster = nil
+			} else {
+				s.cluster = survey(func(string, []byte) (message.Awaiter, error) {
+					return &mockAwaiter{f: func(_ time.Duration) [][]byte { return [][]byte{tc.gathered} }}, nil
+				})
+			}
+
+			out, err := s.Query(tc.query, zero, zero, tc.limit)
+			assert.NoError(t, err)
+			count := 0
+			for range out {
+				count++
+			}
+			assert.Equal(t, tc.count, count)
+		}
+	})
+}
+
+func TestSSD_OnSurvey(t *testing.T) {
+	runSSDTest(func(s *SSD) {
+		s.storeFrame(getNTestMessages(10))
+		zero := time.Unix(0, 0)
+		tests := []struct {
+			name        string
+			query       lookupQuery
+			expectOk    bool
+			expectCount int
+		}{
+			{name: "dummy"},
+			{name: "ssdstore"},
+			{
+				name:        "ssdstore",
+				query:       newLookupQuery(message.Ssid{0, 1}, zero, zero, 1),
+				expectOk:    true,
+				expectCount: 1,
+			},
+			{
+				name:        "ssdstore",
+				query:       newLookupQuery(message.Ssid{0, 1}, zero, zero, 10),
+				expectOk:    true,
+				expectCount: 2,
+			},
+		}
+
+		for _, tc := range tests {
+			q, _ := binary.Marshal(tc.query)
+			resp, ok := s.OnSurvey(tc.name, q)
+			assert.Equal(t, tc.expectOk, ok)
+			if tc.expectOk && ok {
+				msgs, err := message.DecodeFrame(resp)
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectCount, len(msgs))
+			}
+		}
+
+		// Special, wrong payload case
+		_, ok := s.OnSurvey("ssdstore", []byte{})
+		assert.Equal(t, false, ok)
+	})
+
 }
 
 // batch=1         batch/s=43444   msg/s=43444
