1ab6b04c399f83c09602bed30fd8769b4644363c
Cameron Nemo <CameronNemo@users.noreply.github.com>
2019-04-09 19:30:55 -0700

    Default to looking for config in working directory

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	}
 
 	// Process command-line arguments
-	argConfig := flag.String("config", filepath.Join(filepath.Dir(exe), "emitter.conf"), "The configuration file to use for the broker.")
+	argConfig := flag.String("config", "emitter.conf", "The configuration file to use for the broker.")
 	argHelp := flag.Bool("help", false, "Shows the help and usage instead of running the broker.")
 	flag.Parse()
 	if *argHelp {
