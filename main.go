bfd48159e54b4b957acb9db54725da4ae8b6c5af
Cameron Nemo <CameronNemo@users.noreply.github.com>
2019-04-14 14:21:47 -0700

    Default to looking for config in working directory

--- main.go
+++ main.go
@@ -16,14 +16,8 @@ import (
 )
 
 func main() {
-	// Get the directory of the process
-	exe, err := os.Executable()
-	if err != nil {
-		panic(err.Error())
-	}
-
 	// Process command-line arguments
-	argConfig := flag.String("config", filepath.Join(filepath.Dir(exe), "emitter.conf"), "The configuration file to use for the broker.")
+	argConfig := flag.String("config", "emitter.conf", "The configuration file to use for the broker.")
 	argHelp := flag.Bool("help", false, "Shows the help and usage instead of running the broker.")
 	flag.Parse()
 	if *argHelp {
