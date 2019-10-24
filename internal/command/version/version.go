f593ef37edc695288792a6fd5fd1c05e1fb934b0
Roman Atachiants <roman.atachiants@gmail.com>
2019-10-24 23:39:47 +0800

    Added a version command into the executable (#284)
    This PR introduces a version command which simply prints the version of the executable along with the git commit SHA. Use emitter version to run it.

---
+++ internal/command/version/version.go
@@ -0,0 +1,32 @@
+/**********************************************************************************
+* Copyright (c) 2009-2019 Misakai Ltd.
+* This program is free software: you can redistribute it and/or modify it under the
+* terms of the GNU Affero General Public License as published by the  Free Software
+* Foundation, either version 3 of the License, or(at your option) any later version.
+*
+* This program is distributed  in the hope that it  will be useful, but WITHOUT ANY
+* WARRANTY;  without even  the implied warranty of MERCHANTABILITY or FITNESS FOR A
+* PARTICULAR PURPOSE.  See the GNU Affero General Public License  for  more details.
+*
+* You should have  received a copy  of the  GNU Affero General Public License along
+* with this program. If not, see<http://www.gnu.org/licenses/>.
+************************************************************************************/
+
+package version
+
+import (
+	"fmt"
+
+	"github.com/emitter-io/emitter/internal/provider/logging"
+	cli "github.com/jawher/mow.cli"
+)
+
+// Print prints the version
+func Print(version, commit string) func(cmd *cli.Cmd) {
+	return func(cmd *cli.Cmd) {
+		cmd.Spec = ""
+		cmd.Action = func() {
+			logging.LogAction("version", fmt.Sprintf("emitter version v%s, commit %s", version, commit))
+		}
+	}
+}
