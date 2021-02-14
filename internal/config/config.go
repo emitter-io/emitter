5353c571f7772d3ec9353ceab469f2fe576d4a2a
Sergei Vizel <cravler@users.noreply.github.com>
2021-02-14 07:15:25 +0200

    MQTT topic matching strategy (#360)

--- internal/config/config.go
+++ internal/config/config.go
@@ -84,6 +84,7 @@ func New(filename string, stores ...cfg.SecretStore) *Config {
 type Config struct {
 	ListenAddr string              `json:"listen"`             // The API port used for TCP & Websocket communication.
 	License    string              `json:"license"`            // The license file to use for the broker.
+	Matcher    string              `json:"matcher,omitempty"`  // If "mqtt", then topic matching would follow MQTT specification.
 	Debug      bool                `json:"debug,omitempty"`    // The debug mode flag.
 	Limit      LimitConfig         `json:"limit,omitempty"`    // Configuration for various limits such as message size.
 	TLS        *cfg.TLSConfig      `json:"tls,omitempty"`      // The API port used for Secure TCP & Websocket communication.
